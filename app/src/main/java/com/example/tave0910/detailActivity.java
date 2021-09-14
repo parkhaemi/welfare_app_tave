cafac2d87b96e77c722cb28c7f07b390d9d8afb2
parkhaemi <haemipark21@gmail.com>
2021-09-14 11:02:32 +0900

    21.09.13

---
+++ app/src/main/java/com/example/tave0910/detailActivity.java
@@ -0,0 +1,14 @@
+package com.example.tave0910;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class detailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/example/tave0910/homeActivity.java
@@ -0,0 +1,16 @@
+package com.example.tave0910;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+}
